db956e1497b8fe6189255da704fb7dca7a7a80e7
HEYDR <ziyalhaidar@gmail.com>
2026-01-29 12:14:04 +0000

    garnet: Add signing build

--- device.mk
+++ device.mk
@@ -349,6 +349,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# Signing
+-include vendor/lineage-priv/keys/keys.mk
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \

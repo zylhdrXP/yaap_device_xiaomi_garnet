65828525acb3223cbf63f477cb039a02b61b6761
Alhaidar Latif <ziyalhaidar@gmail.com>
2026-02-16 17:53:49 +0000

    Revert "garnet: Update some soong config variables to bool type"
    This reverts commit 4ff528d9516bdb3d6130311be246f5e5e789e173.

--- device.mk
+++ device.mk
@@ -39,7 +39,7 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 
 # Audio
 $(call soong_config_set, android_hardware_audio, run_64bit, true)
-$(call soong_config_set_bool, android_hardware_audio, skip_speaker_layout_channel_mask_field, true)
+$(call soong_config_set, android_hardware_audio, skip_speaker_layout_channel_mask_field, true)
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \

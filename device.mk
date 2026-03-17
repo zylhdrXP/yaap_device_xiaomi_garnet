a9438da5c9ffa3690844f75fe7fb744f0b53a6e1
Alhaidar Latif <ziyalhaidar@gmail.com>
2026-03-17 08:09:51 +0000

    Revert "garnet: move to aidl bootctrl hal"
    This reverts commit 7aad8243b7112c5297a23c01a6fbf2d0f55b9e61.

--- device.mk
+++ device.mk
@@ -116,8 +116,11 @@ PRODUCT_COPY_FILES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot-service.qti \
-    android.hardware.boot-service.qti.recovery
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service \
+    bootctrl.garnet \
+    bootctrl.garnet.recovery
 
 # Camera
 PRODUCT_PACKAGES += \
